96468b7bfc4a6aa5e079001535de8dcd29a979de
Domenic Pittari <dom.pittari@gmail.com>
2026-05-09 11:04:39 +0900

    feat: add badge size variants

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -1,9 +1,9 @@
 import { type BadgeProps, badgeVariants } from './badge.types'
 import { cn } from '@/utils/cn'
 
-export const Badge = ({ className, tone, variant, children, ref, ...props }: BadgeProps) => {
+export const Badge = ({ className, size, tone, variant, children, ref, ...props }: BadgeProps) => {
   return (
-    <div className={cn(badgeVariants({ className, tone, variant }))} ref={ref} {...props}>
+    <div className={cn(badgeVariants({ className, size, tone, variant }))} ref={ref} {...props}>
       {children}
     </div>
   )
